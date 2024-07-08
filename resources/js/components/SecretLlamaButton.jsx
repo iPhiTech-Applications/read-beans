19a15ae79ea85d5742cec42918bb5905fac01d99
faithflores2111 <31542077+faithflores2111@users.noreply.github.com>
2024-07-08 17:17:40 +0800

    Create Chat with SecretLlama button

--- resources/js/components/SecretLlamaButton.jsx
+++ resources/js/components/SecretLlamaButton.jsx
@@ -1,8 +1,29 @@
+import { Affix, HoverCard, rem, Text } from '@mantine/core';
 import { Button } from '@mantine/core';
+import { IconMessage } from '@tabler/icons-react';
 
 export default function SecretLlamaButton() {
   return (
-    <p>test</p>
-    //
+    <Affix position={{ bottom: 32, right: 64 }}>
+      <HoverCard>
+        <HoverCard.Target>
+          <Button
+            size='md'
+            color='blue'
+            variant='light'
+            style={{ width: rem(64), height: rem(64), padding: rem(0) }}
+            radius='xl'
+          >
+            <IconMessage
+              style={{ width: rem(32), height: rem(32) }}
+              stroke={2}
+            />
+          </Button>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Text size='sm'>Chat with SecretLlama</Text>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    </Affix>
   );
 }
