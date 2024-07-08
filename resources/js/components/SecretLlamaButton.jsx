5577b11ac05ead8874d2bbcebe1caa13399fbab8
faithflores2111 <31542077+faithflores2111@users.noreply.github.com>
2024-07-08 16:43:20 +0800

    Initialized Secret Llama button link component

---
+++ resources/js/components/SecretLlamaButton.jsx
@@ -0,0 +1,8 @@
+import { Button } from '@mantine/core';
+
+export default function SecretLlamaButton() {
+  return (
+    <p>test</p>
+    //
+  );
+}
--- resources/js/pages/Dashboard/Index.jsx
+++ resources/js/pages/Dashboard/Index.jsx
@@ -1,12 +1,13 @@
-import Layout from "@/layouts/MainLayout";
-import { usePage } from "@inertiajs/react";
-import { Title } from "@mantine/core";
-import Masonry from "react-masonry-css";
-import OverdueTasks from "./Cards/OverdueTasks";
-import { ProjectCard } from "./Cards/ProjectCard";
-import RecentComments from "./Cards/RecentComments";
-import RecentlyAssignedTasks from "./Cards/RecentlyAssignedTasks";
-import classes from "./css/Index.module.css";
+import Layout from '@/layouts/MainLayout';
+import { usePage } from '@inertiajs/react';
+import { Title } from '@mantine/core';
+import Masonry from 'react-masonry-css';
+import OverdueTasks from './Cards/OverdueTasks';
+import { ProjectCard } from './Cards/ProjectCard';
+import RecentComments from './Cards/RecentComments';
+import RecentlyAssignedTasks from './Cards/RecentlyAssignedTasks';
+import classes from './css/Index.module.css';
+import SecretLlamaButton from '@/components/SecretLlamaButton';
 
 const Dashboard = () => {
   const { projects, overdueTasks, recentlyAssignedTasks, recentComments } = usePage().props;
@@ -19,23 +20,27 @@ const Dashboard = () => {
 
   return (
     <>
-      <Title mb="xl">Dashboard</Title>
+      <Title mb='xl'>Dashboard</Title>
       <Masonry
         breakpointCols={breakpointColumns}
         className={classes.myMasonryGrid}
         columnClassName={classes.myMasonryGridColumn}
       >
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+        {projects.map(project => (
+          <ProjectCard
+            key={project.id}
+            project={project}
+          />
         ))}
         <OverdueTasks tasks={overdueTasks} />
         <RecentlyAssignedTasks tasks={recentlyAssignedTasks} />
         <RecentComments comments={recentComments} />
+        <SecretLlamaButton />
       </Masonry>
     </>
   );
 };
 
-Dashboard.layout = (page) => <Layout title="Dashboard">{page}</Layout>;
+Dashboard.layout = page => <Layout title='Dashboard'>{page}</Layout>;
 
 export default Dashboard;
